67ce3a204301f0191946307b9441eee3640e44f5
jayme17 <jjones@chicagotechacademy.org>
2014-01-31 13:11:47 -0600

    JJ's New Second App
    Thanks Obama

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+AppNumber2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppNumber2::Application.config.secret_token = 'da77dda2a4840db1318f5399f1a02c6b83870633d82d7847d077e2856af1500cf97754b8defbc9098ca893d7f8f29bb709a3f7a5b125d7ecbdaa34b078b11123'
